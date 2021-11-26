775b473812409164cdb5462c411f868211778b1a
Alex Bishop <alexbishopffxiv@gmail.com>
2021-11-25 19:02:47 -0800

    Initial implementation

---
+++ src/bait-data.ts
@@ -0,0 +1,44 @@
+export const baitData = {
+  "Galadion Bay": {
+    default: "plump",
+    day: "ragworm",
+    sunset: "plump",
+    night: "krill",
+  },
+  "Southern Merlthor": {
+    default: "krill",
+    day: "krill",
+    sunset: "ragworm",
+    night: "plump",
+  },
+  "Northern Merlthor": {
+    default: "ragworm",
+    day: "plump",
+    sunset: "ragworm",
+    night: "krill",
+  },
+  "Rhotano Sea": {
+    default: "plump",
+    day: "plump",
+    sunset: "ragworm",
+    night: "krill",
+  },
+  "The Cieldalaes": {
+    default: "ragworm",
+    day: "krill",
+    sunset: "plump",
+    night: "krill",
+  },
+  "Bloodbrine Sea": {
+    default: "krill",
+    day: "ragworm",
+    sunset: "plump",
+    night: "krill",
+  },
+  "Rothlyt Sound": {
+    default: "plump",
+    day: "krill",
+    sunset: "krill",
+    night: "krill",
+  },
+};
---
+++ src/route-data.ts
@@ -0,0 +1,722 @@
+export const routeData = [
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Galadion Bay", "day"],
+    ["Southern Merlthor", "sunset"],
+    ["Rhotano Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Northern Merlthor", "night"],
+    ["Bloodbrine Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "sunset"],
+    ["Rhotano Sea", "night"],
+    ["Rothlyt Sound", "day"],
+  ],
+  [
+    ["Southern Merlthor", "sunset"],
+    ["Galadion Bay", "night"],
+    ["Northern Merlthor", "day"],
+  ],
+  [
+    ["Galadion Bay", "sunset"],
+    ["Southern Merlthor", "night"],
+    ["Rhotano Sea", "day"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Northern Merlthor", "day"],
+    ["Bloodbrine Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "night"],
+    ["Rhotano Sea", "day"],
+    ["Rothlyt Sound", "sunset"],
+  ],
+  [
+    ["Southern Merlthor", "night"],
+    ["Galadion Bay", "day"],
+    ["Northern Merlthor", "sunset"],
+  ],
+  [
+    ["Galadion Bay", "night"],
+    ["Southern Merlthor", "day"],
+    ["Rhotano Sea", "sunset"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Northern Merlthor", "sunset"],
+    ["Bloodbrine Sea", "night"],
+  ],
+  [
+    ["The Cieldalaes", "day"],
+    ["Rhotano Sea", "sunset"],
+    ["Rothlyt Sound", "night"],
+  ],
+  [
+    ["Southern Merlthor", "day"],
+    ["Galadion Bay", "sunset"],
+    ["Northern Merlthor", "night"],
+  ],
+] as const;
